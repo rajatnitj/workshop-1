825634205e6a4f024880a9ecff9a307dc87be30c
pratyasha123 <56603357+pratyasha123@users.noreply.github.com>
2019-10-15 23:21:01 +0530

    Add files via upload

---
+++ prog2.c.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+    float c,f;
+    printf("enter the value of c");
+    scanf("%f",&c);
+    f=9/5*c+32;
+    printf("the value of f is %f",f);
+    return 0;
+}
